5b97dd6d6bac8798a73a965d796cf0cf2e0c2edb
G10on <jitenparwani1c17@gmail.com>
2022-07-04 21:49:31 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+import * as tf from '@tensorflow/tfjs'
+
+
+var modelo = null;
+
+//Cargar modelo
+(async () => {
+  console.log("Cargando modelo...");
+  modelo = await tf.loadLayersModel("model.json");
+  console.log("Modelo cargado...");
+})();
+
+
+
+function call(){
+  console.log('Hello!');
+  alert('Hey')
+  return false;
+}
+*/
